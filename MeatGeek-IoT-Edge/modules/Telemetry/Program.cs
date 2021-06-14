c3dba1e0483ab3ae9f13ffee9345be8599289670
Steve Bargelt <steve@bargelt.com>
2021-06-14 07:51:08 -0700

    standardizing json capitalization

--- MeatGeek-IoT-Edge/modules/Telemetry/Program.cs
+++ MeatGeek-IoT-Edge/modules/Telemetry/Program.cs
@@ -166,6 +166,7 @@ namespace Telemetry
                 eventMessage.ContentEncoding = "UTF-8";
                 eventMessage.Properties.Add("correlationId", correlationId);
                 eventMessage.Properties.Add("sequenceNumber", count.ToString());
+                eventMessage.Properties.Add("SessionId", SessionID);
                 
                 try
                 {
@@ -313,28 +314,47 @@ namespace Telemetry
 
     public class SmokerStatus
     {
-        [JsonProperty("id")] public string Id { get; set; }
-        [JsonProperty] public int? ttl { get; set; }
-        [JsonProperty] public string SmokerId { get; set; }
-        [JsonProperty] public string SessionId { get; set; }
-        [JsonProperty] public bool AugerOn { get; set; }
-        [JsonProperty] public bool BlowerOn { get; set; }
-        [JsonProperty] public bool IgniterOn { get; set; }
-        [JsonProperty] public Temps Temps { get; set; }
-        [JsonProperty] public bool FireHealthy { get; set; }
-        [JsonProperty] public string Mode { get; set; }
-        [JsonProperty] public int SetPoint { get; set; }
-        [JsonProperty] public DateTime ModeTime { get; set; }
-        [JsonProperty] public DateTime CurrentTime { get; set; }
+        [JsonProperty("id")] 
+        public string Id { get; set; }
+        [JsonProperty] 
+        public int? ttl { get; set; }
+        [JsonProperty("smokerId")] 
+        public string SmokerId { get; set; }
+        [JsonProperty("sessionId")] 
+        public string SessionId { get; set; }
+        [JsonProperty("type")] 
+        public string Type { get; set; }
+        [JsonProperty("augerOn")] 
+        public bool AugerOn { get; set; }
+        [JsonProperty("blowerOn")] 
+        public bool BlowerOn { get; set; }
+        [JsonProperty("igniterOn")] 
+        public bool IgniterOn { get; set; }
+        [JsonProperty("temps")] 
+        public Temps Temps { get; set; }
+        [JsonProperty("fireHealthy")] 
+        public bool FireHealthy { get; set; }
+        [JsonProperty("mode")] 
+        public string Mode { get; set; }
+        [JsonProperty("setPoint")] 
+        public int SetPoint { get; set; }
+        [JsonProperty("modeTime")] 
+        public DateTime ModeTime { get; set; }
+        [JsonProperty("currentTime")] 
+        public DateTime CurrentTime { get; set; }
     }
     public class Temps
     {
+        [JsonProperty("grillTemp")] 
         public double GrillTemp { get; set; }
+        [JsonProperty("probe1Temp")] 
         public double Probe1Temp { get; set; }
+        [JsonProperty("probe2Temp")] 
         public double Probe2Temp { get; set; }
+        [JsonProperty("probe3Temp")] 
         public double Probe3Temp { get; set; }
+        [JsonProperty("probe4Temp")] 
         public double Probe4Temp { get; set; }
-
     }
 
     }
